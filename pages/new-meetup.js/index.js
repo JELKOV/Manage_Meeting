d8a485b977fac4143be48fcfc12580bdf13eb9f5
JAEHO AHN <ajh4234@gmail.com>
2025-04-03 16:58:33 +0900

    Dev Log #3: New Meetup Form 구현 및 데이터 수집

--- pages/new-meetup.js/index.js
+++ pages/new-meetup.js/index.js
@@ -1,1 +1,12 @@
 // 새 모임 약속 페이지
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+
+function NewMeetupPage() {
+  function addMeetupHandler(enteredMeetupData) {
+    console.log(enteredMeetupData);
+  }
+
+  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+}
+
+export default NewMeetupPage;
