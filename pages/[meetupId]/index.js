be8de829c300af42592ddb1185d12a69da98dbaf
JAEHO AHN <ajh4234@gmail.com>
2025-04-03 17:39:34 +0900

    Dev Log #4: 동적 라우팅 및 Meetup 상세 페이지 구현

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://lh3.googleusercontent.com/gps-cs-s/AB5caB9gaZ7QemAEFHjKjnWRn9r5tyg0dub7uO37nk2WWZ-8OLxI6FHXSID95SJFwOSgXR-oJQsr6Na6h5K2JDNHmaaWw-sOEoxvA6j3BmL6j0XRPOg7kuhXbUuLNhcHtvomKDEQcvqUYA=s1360-w1360-h1020"
+      title="신촌 저녁 번개 모임"
+      address="서울 서대문구 연세로 12길 34, 커피빈 신촌점"
+      description="퇴근 후 가볍게 만나 커피 한 잔하며 이야기 나눠요!"
+    />
+  );
+}
+
+export default MeetupDetails;
