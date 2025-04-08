af7c6087b92bb2f7b01b930d22a1262b3e393f1d
JAEHO AHN <ajh4234@gmail.com>
2025-04-08 15:18:32 +0900

    Dev_Log#17: meetup 등록 시 creatorId 저장 기능 구현

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,8 +1,19 @@
 // pages/api/new-meetup.js
+import { getServerSession } from "next-auth";
 import clientPromise from "../../lib/db";
+import { authOptions } from "./auth/[...nextauth]";
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
+    // 사용자 세션 확인
+    const session = await getServerSession(res, req, authOptions);
+
+    if (!session) {
+      return res
+        .status(401)
+        .json({ message: "인증된 사용자만 사용할수 있습니다." });
+    }
+
     const { title, image, address, description } = req.body;
 
     try {
@@ -15,11 +26,17 @@ export default async function handler(req, res) {
         image,
         address,
         description,
+        creatorId: session.user.id,
+        createdAt: new Date(),
       });
 
-      res.status(201).json({ message: "Meetup inserted!", id: result.insertedId });
+      res
+        .status(201)
+        .json({ message: "Meetup inserted!", id: result.insertedId });
     } catch (error) {
-      res.status(500).json({ message: "DB 저장 중 오류 발생", error: error.message });
+      res
+        .status(500)
+        .json({ message: "DB 저장 중 오류 발생", error: error.message });
     }
   } else {
     res.status(405).json({ message: "허용되지 않은 요청입니다." });
